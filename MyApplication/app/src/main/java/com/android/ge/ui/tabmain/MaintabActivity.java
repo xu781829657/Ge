8b5ef7f83ad4437eb8f600b99030a13fca35c539
许登旺 <781829657@qq.com>
2017-03-17 22:29:44 +0800

    主tab页面

--- MyApplication/app/src/main/java/com/android/ge/ui/tabmain/MaintabActivity.java
+++ MyApplication/app/src/main/java/com/android/ge/ui/tabmain/MaintabActivity.java
@@ -1,15 +1,22 @@
 package com.android.ge.ui.tabmain;
 
+import android.widget.TextView;
+
+
+import com.android.ge.R;
 import com.android.ge.ui.base.CommonBaseActivity;
+
+import butterknife.Bind;
 
 /**
  * Created by xudengwang on 2017/3/14.
  */
 
-public class MantabActivity extends CommonBaseActivity{
+public class MaintabActivity extends CommonBaseActivity{
+    String value = "1/10";
     @Override
     protected int getContentViewId() {
-        return 0;
+        return R.layout.activity_main_tab ;
     }
 
     @Override
---
+++ MyApplication/app/src/main/java/com/android/ge/widgets/view/NoScrollViewPager.java
@@ -0,0 +1,41 @@
+package com.bricks.widgets.viewpager;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * 禁止左右滑的ViewPager
+ * Created by zhangmingxue on 2015/9/23.
+ */
+public class NoScrollViewPager extends ViewPager {
+
+    private boolean noScroll = true;
+
+    public NoScrollViewPager(Context context) {
+        super(context);
+    }
+
+    public NoScrollViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent ev) {
+        if (noScroll) {
+            return false;
+        } else {
+            return super.onInterceptTouchEvent(ev);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent ev) {
+        if (noScroll) {
+            return false;
+        } else {
+            return super.onTouchEvent(ev);
+        }
+    }
+}
