cca4b019df2eacb16a1a14169bcea69adc4230b4
许登旺 <781829657@qq.com>
2017-03-29 00:00:13 +0800

    登录页

---
+++ MyApplication/app/src/main/java/com/android/ge/ui/login/LoginActivity.java
@@ -0,0 +1,21 @@
+package com.android.ge.ui.login;
+
+import com.android.ge.R;
+import com.android.ge.ui.base.CommonBaseActivity;
+
+/**
+ * Created by xudengwang on 17/3/28.
+ */
+
+public class LoginActivity extends CommonBaseActivity{
+
+    @Override
+    protected int getContentViewId() {
+        return R.layout.activity_login;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
