3836ead21fc633f1e2d42d51c9b4effa021639ba
许登旺 <781829657@qq.com>
2017-07-13 05:15:37 +0800

    课程详情简介

---
+++ MyApplication/app/src/main/java/com/android/ge/ui/course/CourseIntroFragment.java
@@ -0,0 +1,21 @@
+package com.android.ge.ui.course;
+
+import android.support.v4.app.Fragment;
+
+import com.android.ge.ui.base.CommonBaseFragment;
+
+/**
+ * Created by xudengwang on 17/7/12.
+ */
+
+public class CourseIntroFragment extends CommonBaseFragment{
+    @Override
+    public int getContentViewId() {
+        return 0;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
