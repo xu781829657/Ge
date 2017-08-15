cde98fd7fa7de557239efec9d5950b522febad5c
xudengwang <xudengwang123>
2017-08-15 16:40:27 +0800

    补充英文；任务entryid改成任务id

--- MyApplication/app/src/main/java/com/android/ge/controller/adapter/TaskDetailAdapter.java
+++ MyApplication/app/src/main/java/com/android/ge/controller/adapter/TaskDetailAdapter.java
@@ -115,14 +115,14 @@ public class TaskDetailAdapter extends BaseCommonAdapter<TaskDetailBean> impleme
             Bundle bundle = new Bundle();
             bundle.putString(CommonConstant.PARAM_COURSE_ID, taskDetailBean.courses.getId() + "");
             bundle.putString(CommonConstant.PARAM_ENTRY_TYPE, CommonConstant.TYPE_MISSIONS);
-            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.courses.getId() + "");
+            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.getId() + "");
             gotoActivity(CourseWebActivity.class, bundle);
         } else if (CommonConstant.TASK_COURSE_TYPE_QUIZ.equalsIgnoreCase(taskDetailBean.getDetail_type()) &&
                 taskDetailBean.examinations != null) {
             Bundle bundle = new Bundle();
             bundle.putString(CommonConstant.PARAM_EXAM_ID, taskDetailBean.examinations.getId() + "");
             bundle.putString(CommonConstant.PARAM_ENTRY_TYPE, CommonConstant.TYPE_MISSIONS);
-            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.examinations.getId() + "");
+            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.getId() + "");
             if (taskDetailBean.getProgress() == 100) {
                 bundle.putString(CommonConstant.PARAM_ISFINISH, "1");
             } else {
@@ -134,7 +134,7 @@ public class TaskDetailAdapter extends BaseCommonAdapter<TaskDetailBean> impleme
             Bundle bundle = new Bundle();
             bundle.putString(CommonConstant.PARAM_QUESTION_ID, taskDetailBean.questionnaire.getId() + "");
             bundle.putString(CommonConstant.PARAM_ENTRY_TYPE, CommonConstant.TYPE_MISSIONS);
-            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.questionnaire.getId() + "");
+            bundle.putString(CommonConstant.PARAM_ENTRY_ID, taskDetailBean.getId() + "");
             if (taskDetailBean.getProgress() == 100) {
                 bundle.putString(CommonConstant.PARAM_ISFINISH, "1");
             } else {
