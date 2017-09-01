642a46cc733f41236c3f5d6b2d39b1f116e2ef77
许登旺 <781829657@qq.com>
2017-09-01 08:15:42 +0800

    3.6.0打包

--- MyApplication/app/src/main/java/com/android/ge/constant/AppConfig.java
+++ MyApplication/app/src/main/java/com/android/ge/constant/AppConfig.java
@@ -8,5 +8,5 @@ public class AppConfig {
     public static final int ENVIRONMENT_PRO = 0;//生产环境
     public static final int ENVIRONMENT_DEV = 1;//测试环境
     //
-    public static final int ENVIRONMENT_DEFAULT = ENVIRONMENT_DEV;
+    public static final int ENVIRONMENT_DEFAULT = ENVIRONMENT_PRO;
 }
